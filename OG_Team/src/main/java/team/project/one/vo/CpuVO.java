a719861601863af23107d3b3a4a467718f790656
MunGukMa <55041779+MunGukMa@users.noreply.github.com>
2019-09-24 19:00:22 +0900

    Update CpuVO.java

--- OG_Team/src/main/java/team/project/one/vo/CpuVO.java
+++ OG_Team/src/main/java/team/project/one/vo/CpuVO.java
@@ -2,41 +2,40 @@ package team.project.one.vo;
 
 import java.io.Serializable;
 
-public class CpuVO implements Serializable
+package parshing;
+
+public class CpuVO 
 {
 	int cpunum;
 	String cpuname;
-	String price;
-	String cpuMark;
-	String cpuval;
-	String threadMark;
-	String threadVal;
-	String tdp;
-	String powerf;
-	String date;
+	String cpucode;
+	String cores;
+	String clock;
 	String socket;
-	String category;
-	String rank;
-		
-	public PMCpuVO() {
+	String process;
+	String l3cache;
+	String tdp;
+	String released;
+	int point;
+	
+	public TechCpuVO() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public PMCpuVO(String cpuname, String price, String cpuMark, String cpuval, String threadMark, String threadVal,
-			String tdp, String powerf, String date, String socket, String category, String rank) {
+	public TechCpuVO(int cpunum, String cpuname, String cpucode, String cores, String clock, String socket,
+			String process, String l3cache, String tdp, String released, int point) {
 		super();
+		this.cpunum = cpunum;
 		this.cpuname = cpuname;
-		this.price = price;
-		this.cpuMark = cpuMark;
-		this.cpuval = cpuval;
-		this.threadMark = threadMark;
-		this.threadVal = threadVal;
-		this.tdp = tdp;
-		this.powerf = powerf;
-		this.date = date;
+		this.cpucode = cpucode;
+		this.cores = cores;
+		this.clock = clock;
 		this.socket = socket;
-		this.category = category;
-		this.rank = rank;
+		this.process = process;
+		this.l3cache = l3cache;
+		this.tdp = tdp;
+		this.released = released;
+		this.point = point;
 	}
 
 	public int getCpunum() {
@@ -55,68 +54,28 @@ public class CpuVO implements Serializable
 		this.cpuname = cpuname;
 	}
 
-	public String getPrice() {
-		return price;
+	public String getCpucode() {
+		return cpucode;
 	}
 
-	public void setPrice(String price) {
-		this.price = price;
+	public void setCpucode(String cpucode) {
+		this.cpucode = cpucode;
 	}
 
-	public String getCpuMark() {
-		return cpuMark;
+	public String getCores() {
+		return cores;
 	}
 
-	public void setCpuMark(String cpuMark) {
-		this.cpuMark = cpuMark;
+	public void setCores(String cores) {
+		this.cores = cores;
 	}
 
-	public String getCpuval() {
-		return cpuval;
+	public String getClock() {
+		return clock;
 	}
 
-	public void setCpuval(String cpuval) {
-		this.cpuval = cpuval;
-	}
-
-	public String getThreadMark() {
-		return threadMark;
-	}
-
-	public void setThreadMark(String threadMark) {
-		this.threadMark = threadMark;
-	}
-
-	public String getThreadVal() {
-		return threadVal;
-	}
-
-	public void setThreadVal(String threadVal) {
-		this.threadVal = threadVal;
-	}
-
-	public String getTdp() {
-		return tdp;
-	}
-
-	public void setTdp(String tdp) {
-		this.tdp = tdp;
-	}
-
-	public String getPowerf() {
-		return powerf;
-	}
-
-	public void setPowerf(String powerf) {
-		this.powerf = powerf;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
+	public void setClock(String clock) {
+		this.clock = clock;
 	}
 
 	public String getSocket() {
@@ -127,32 +86,54 @@ public class CpuVO implements Serializable
 		this.socket = socket;
 	}
 
-	public String getCategory() {
-		return category;
+	public String getProcess() {
+		return process;
 	}
 
-	public void setCategory(String category) {
-		this.category = category;
+	public void setProcess(String process) {
+		this.process = process;
 	}
 
-	public String getRank() {
-		return rank;
+	public String getL3cache() {
+		return l3cache;
 	}
 
-	public void setRank(String rank) {
-		this.rank = rank;
+	public void setL3cache(String l3cache) {
+		this.l3cache = l3cache;
+	}
+
+	public String getTdp() {
+		return tdp;
+	}
+
+	public void setTdp(String tdp) {
+		this.tdp = tdp;
+	}
+
+	public String getReleased() {
+		return released;
+	}
+
+	public void setReleased(String released) {
+		this.released = released;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
 	}
 
 	@Override
 	public String toString() {
-		return "PMCpuVO [cpunum=" + cpunum + ", cpuname=" + cpuname + ", price=" + price + ", cpuMark=" + cpuMark
-				+ ", cpuval=" + cpuval + ", threadMark=" + threadMark + ", threadVal=" + threadVal + ", tdp=" + tdp
-				+ ", powerf=" + powerf + ", date=" + date + ", socket=" + socket + ", category=" + category + ", rank="
-				+ rank + "]";
+		return "TechCpuVO [cpunum=" + cpunum + ", cpuname=" + cpuname + ", cpucode=" + cpucode + ", cores=" + cores
+				+ ", clock=" + clock + ", socket=" + socket + ", process=" + process + ", l3cache=" + l3cache + ", tdp="
+				+ tdp + ", released=" + released + ", point=" + point + "]";
 	}
-
 	
 	
 	
-		
 }
+
