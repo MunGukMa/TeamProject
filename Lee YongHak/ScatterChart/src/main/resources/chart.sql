52b9ec252602d97f22fc150ee55a7d08c2018f54
MunGukMa <55041779+MunGukMa@users.noreply.github.com>
2019-09-24 17:23:51 +0900

    directory clean

--- Lee YongHak/ScatterChart/src/main/resources/chart.sql
+++ Lee YongHak/ScatterChart/src/main/resources/chart.sql
@@ -1,21 +1,23 @@
-create table chart
-(
-gamename VARCHAR2(30) primary key,
-gamelevel number not null,
-pclevel number not null, 
-imagelink varchar2(100) not null
-)
+create table gamelist(
+gamename varchar2(20) primary key,
+gamelevel number,
+pclevel number,
+imagelink varchar2(100) not null,
+gamecpu varchar2(50),
+gameram varchar2(50),
+gamegpu varchar2(50),
+gameaddop varchar2(100),
+memo varchar2(100))
 
-insert into chart values('lol',9,2,'resources/icon/LOL.jpg');
+insert into gamelist values('lol',9,2,'resources/icon/LOL.jpg','Intel Core I5-8600','16gb','gtx 1060',null,null);
 
-insert into chart values('overwatch',5,5,'resources/icon/overwatch.jpg');
+insert into gamelist values('overwatch',5,5,'resources/icon/overwatch.jpg','Intel Core I5-8600','16gb','gtx 1060',null,null);
 
-insert into chart values('battleground',6,4,'resources/icon/playerunknowns_battlegrounds.jpg');
+insert into gamelist values('battleground',6,4,'resources/icon/playerunknowns_battlegrounds.jpg','Intel Core I5-8600','16gb','gtx 1060',null,null);
 
-insert into chart values('dota2',2,3,'resources/icon/dota_2.jpg');
+insert into gamelist values('dota2',2,3,'resources/icon/dota_2.jpg','Intel Core I5-8600','16gb','gtx 1060',null,null);
 
-insert into chart values('wow',7,1,'resources/icon/world_of_warcraft.jpg');
+insert into gamelist values('wow',7,1,'resources/icon/world_of_warcraft.jpg','Intel Core I5-8600','16gb','gtx 1060',null,null);
 
-insert into chart values('rainbow6',4,5,'resources/icon/tom_clancys_rainbow_six_siege.jpg');
-
+insert into gamelist values('rainbow6',4,5,'resources/icon/tom_clancys_rainbow_six_siege.jpg','Intel Core I5-8600','16gb','gtx 1060',null,null);
 commit;
