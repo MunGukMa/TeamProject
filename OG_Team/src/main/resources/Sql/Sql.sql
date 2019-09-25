f43f277d4c0768bb80f0bd0f2aa39b09449b2e04
MunGukMa <55041779+MunGukMa@users.noreply.github.com>
2019-09-25 19:18:03 +0900

    Ma_0925_TabSql_up
    add  power,steam table

--- OG_Team/src/main/resources/Sql/Sql.sql
+++ OG_Team/src/main/resources/Sql/Sql.sql
@@ -68,3 +68,41 @@ name varchar2(100) not null,
 spec varchar2(300) not null,
 src_link varchar2(300) not null
 );
+
+create table mainboard
+(
+    num number primary key,
+    name varchar2(100) not null,
+    sockets varchar2(100) not null,
+    formFactor varchar2(100) not null,
+    chipSet varchar2(100) not null,
+    RAM varchar2(50) not null,
+    releaseDate varchar2(50) not null,
+    audioChip varchar2(50) not null,
+    usb2 varchar2(20) not null,
+    usb3 varchar2(20) not null,
+    sata varchar2(20) not null
+);
+
+create table power
+(
+    num number primary key,
+    name varchar2(200) not null,
+    price varchar2(20) not null,
+    power varchar2(20) not null,
+    output varchar2(20) not null,
+    fanSize varchar2(20) not null,
+    fanNum varchar2(10) not null,
+    atx varchar2(10) not null,
+    sata varchar2(10) not null,
+    connecter varchar2(200) not null,
+    etc varchar2(500) not null,
+    releaseDate varchar2(20) not null
+);
+
+create table steam
+(
+    num number primary key,
+    appid varchar2(100) not null,
+    name varchar2(200) not null
+);
