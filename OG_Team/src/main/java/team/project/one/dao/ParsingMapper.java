9cfc7aa1d20678ef1d6b69e88ae6d75a501e7eef
MunGukMa <55041779+MunGukMa@users.noreply.github.com>
2019-09-30 14:27:22 +0900

    Ma_0930_Update_add
    add other's method

--- OG_Team/src/main/java/team/project/one/dao/ParsingMapper.java
+++ OG_Team/src/main/java/team/project/one/dao/ParsingMapper.java
@@ -2,12 +2,32 @@ package team.project.one.dao;
 
 import java.util.ArrayList;
 
+import team.project.one.vo.CaseVO;
+import team.project.one.vo.CpuVO;
+import team.project.one.vo.GpuVO;
 import team.project.one.vo.MainBoardVO;
+import team.project.one.vo.PowerVO;
+import team.project.one.vo.RamVO;
+import team.project.one.vo.SteamVO;
 
 public interface ParsingMapper {
 
+	public int getnumber();
+
+	public int getCnt(String str);
+	
 	public void insertMainBoard(ArrayList<MainBoardVO> list);
 
-	public int getnumber();
+	public void insertPower(ArrayList<PowerVO> list);
+
+	public void insertCpu(ArrayList<CpuVO> cpulist);
+
+	public void insertCase(ArrayList<CaseVO> caselist);
+
+	public void insertRam(ArrayList<RamVO> list);
+
+	public void insertGpu(ArrayList<GpuVO> list);
+
+	public void insertSteam(ArrayList<SteamVO> list);
 
 }
