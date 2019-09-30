1b9afb055b187d627b26e75168a2176a369518f1
MunGukMa <55041779+MunGukMa@users.noreply.github.com>
2019-09-30 14:25:25 +0900

    Ma_0930_Add_NewMapper
    Add Autocomplete Mapper

---
+++ OG_Team/src/main/java/team/project/one/dao/AutoMapper.java
@@ -0,0 +1,11 @@
+package team.project.one.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface AutoMapper {
+
+
+	ArrayList<String> getlist(HashMap<String, String> templist);
+
+}
