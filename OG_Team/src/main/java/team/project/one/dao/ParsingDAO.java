18f6ce460a66ed67dadeab94d844f839b36a89c1
MunGukMa <55041779+MunGukMa@users.noreply.github.com>
2019-09-30 14:26:39 +0900

    Ma_0930_Update_Method
    add other material's parsing method

--- OG_Team/src/main/java/team/project/one/dao/ParsingDAO.java
+++ OG_Team/src/main/java/team/project/one/dao/ParsingDAO.java
@@ -6,30 +6,75 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import team.project.one.vo.CaseVO;
+import team.project.one.vo.CpuVO;
+import team.project.one.vo.GpuVO;
 import team.project.one.vo.MainBoardVO;
+import team.project.one.vo.PowerVO;
+import team.project.one.vo.RamVO;
+import team.project.one.vo.SteamVO;
 
 @Repository
 public class ParsingDAO {
 
 	@Autowired
-	private SqlSession session;
+	SqlSession session;
 	
-	
-	public void insertMainBoard(ArrayList<MainBoardVO> list) 
-	{
-		for(MainBoardVO mbvo:list)
-		{
-			System.out.println(mbvo);
-		}
-		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
-		mapper.insertMainBoard(list);
-	}
-
-
 	public int getnumber() 
 	{
 		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
 		return mapper.getnumber();
+	}
+	
+	
+	public int getCnt(String str) 
+	{
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		return mapper.getCnt(str);
+	}
+	
+	public void insertMainBoard(ArrayList<MainBoardVO> list) 
+	{
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		mapper.insertMainBoard(list);
+	}
+
+	public void insertPower(ArrayList<PowerVO> list) {
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		mapper.insertPower(list);
+	}
+
+
+	public void insertCpu(ArrayList<CpuVO> cpulist) {
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		mapper.insertCpu(cpulist);
+	}
+
+
+	public void insertCase(ArrayList<CaseVO> caselist) {
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		mapper.insertCase(caselist);
+		
+	}
+
+
+	public void insertRam(ArrayList<RamVO> list) {
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		mapper.insertRam(list);
+		
+	}
+
+
+	public void insertGpu(ArrayList<GpuVO> list) {
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		mapper.insertGpu(list);
+	}
+
+
+	public void insertSteam(ArrayList<SteamVO> list) {
+		ParsingMapper mapper = session.getMapper(ParsingMapper.class);
+		mapper.insertSteam(list);
+		
 	}
 
 }
