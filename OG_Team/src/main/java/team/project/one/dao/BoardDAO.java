51ae508e4b2082799c40802f4029a74f6b9b707b
MunGukMa <55041779+MunGukMa@users.noreply.github.com>
2019-10-05 17:57:07 +0900

    Ma_1005_update_Boarddao

--- OG_Team/src/main/java/team/project/one/dao/BoardDAO.java
+++ OG_Team/src/main/java/team/project/one/dao/BoardDAO.java
@@ -121,4 +121,68 @@ public class BoardDAO {
 		
 		return result;
 	}
+	
+	public ArrayList<BoardVO> boardList(HashMap<String, String> map, int startRecord, int countPerPage){
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		RowBounds rb = new RowBounds(startRecord, countPerPage);
+		return mapper.boardList(map, rb);
+	}
+	
+	public BoardVO boardRead(int b_num) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		mapper.hitCount(b_num);
+		return mapper.boardRead(b_num);
+	}
+	
+	public void hitCount(int b_num) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		mapper.hitCount(b_num);
+	}
+	
+	public int boardWrite(BoardVO vo) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.boardWrite(vo);
+	}
+	
+	public int boardDelete(BoardVO vo) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.boardDelete(vo);
+	}
+	
+	public int boardUpdate(BoardVO vo) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.boardUpdate(vo);
+	}
+
+	public int getTotal(HashMap<String, String> map) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.getTotal(map);
+	}
+	
+	public ArrayList<CommentVO> commentList(int b_num){
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.commentList(b_num);
+	}
+	
+	public void commentWrite(CommentVO vo) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		mapper.commentWrite(vo);
+	}
+	
+	public void commentUpdate(CommentVO vo) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		mapper.commentUpdate(vo);
+	}
+	
+	public ArrayList<PCEstimateVO> pcsetList(String fitc_id){
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.pcsetList(fitc_id);
+	}
+
+	public PCEstimateVO getpc(PCEstimateVO vo) {
+		BoardMapper mapper = sqlSession.getMapper(BoardMapper.class);
+		return mapper.getpc(vo);
+	}
+	
+	
 }
