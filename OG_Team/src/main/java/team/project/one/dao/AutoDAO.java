b8d1dcc2d15d2a9ccea4214336608828bfd27a6e
gredius <55075010+gredius@users.noreply.github.com>
2019-10-07 15:05:54 +0900

    SH_1007_getlist Update
    2019.10.07 SHC
    getlist Updated

--- OG_Team/src/main/java/team/project/one/dao/AutoDAO.java
+++ OG_Team/src/main/java/team/project/one/dao/AutoDAO.java
@@ -2,6 +2,7 @@ package team.project.one.dao;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +15,38 @@ public class AutoDAO {
 	@Autowired
 	SqlSession session;
 	
-	
-	public ArrayList<String> getlist(String keyvalue) 
-	{
+	public ArrayList<String> getlist(String keyvalue, String key) {
 		AutoMapper mapper = session.getMapper(AutoMapper.class);
-		HashMap<String, String> templist = new HashMap<>();
-		String[] temp = keyvalue.split(" ");
-		for(int i =0; i<temp.length;i++)
+		Map<String, String[]> templist = new HashMap<String,String[]>();
+		String[] temp = keyvalue.split("");
+		templist.put("keyvalue", temp);
+		
+		if(key ==null){
+			return mapper.getlistall(templist);
+		} else if(key.equals("fit_cpu"))
 		{
-			templist.put("keyvalue", keyvalue);
+			return mapper.getlistcpu(templist);
+		} else if(key.equals("fit_gpu"))
+		{
+			return mapper.getlistgpu(templist);
+		} else if(key.equals("fit_ram"))
+		{
+			return mapper.getlistram(templist);
+		} else if(key.equals("fit_mainboard"))
+		{
+			return mapper.getlistmainboard(templist);
+		} else if(key.equals("fit_case"))
+		{
+			return mapper.getlistcase(templist);
+		} else if(key.equals("fit_power"))
+		{
+			return mapper.getlistpower(templist);
+		} else if(key.equals("fit_steam"))
+		{
+			return mapper.getliststeam(templist);
+		} else 
+		{
+			return mapper.getlistall(templist);
 		}
-		return mapper.getlist(templist);
 	}
-
 }
