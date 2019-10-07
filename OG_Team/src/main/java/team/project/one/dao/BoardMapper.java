1acc3955cd48642350ea889194140922146b28a1
yhl88 <55075025+yhl88@users.noreply.github.com>
2019-10-07 14:29:29 +0900

    yhl_1007_boarddao_update
    yhl_1007_boarddao_update

--- OG_Team/src/main/java/team/project/one/dao/BoardMapper.java
+++ OG_Team/src/main/java/team/project/one/dao/BoardMapper.java
@@ -12,16 +12,17 @@ import team.project.one.vo.PCEstimateVO;
 
 public interface BoardMapper {
 	public ArrayList<BoardVO> boardList(HashMap<String, String> map, RowBounds rb);
-	public BoardVO boardRead(int b_num);
-	public void hitCount(int b_num);
+	public BoardVO boardRead(int fit_boardnum);
+	public void hitCount(int fit_boardnum);
 	public int boardWrite(BoardVO vo);
 	public int boardDelete(BoardVO vo);
 	public int boardUpdate(BoardVO vo);
 	public int getTotal(HashMap<String, String> map);
-	public ArrayList<CommentVO> commentList(int b_num);
+	public ArrayList<CommentVO> commentList(int fit_boardnum);
 	public void commentWrite(CommentVO vo);
 	public void commentUpdate(CommentVO vo);
 	public void commentDelete(CommentVO vo);
-	public ArrayList<PCEstimateVO> pcsetList(String fitc_id);
+	public ArrayList<PCEstimateVO> pcsetList(String fit_userid);
 	public PCEstimateVO getpc(PCEstimateVO vo);
+	public PCEstimateVO getPcOne(BoardVO vo);
 }
