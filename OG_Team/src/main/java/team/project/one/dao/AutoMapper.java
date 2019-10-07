ac342c37af93d7cbd8dd1812d6b2f9977634d268
gredius <55075010+gredius@users.noreply.github.com>
2019-10-07 15:08:00 +0900

    SH_1007_AutoMapper Update
    2019.10.07 SHC
    AutoMapper Updated

--- OG_Team/src/main/java/team/project/one/dao/AutoMapper.java
+++ OG_Team/src/main/java/team/project/one/dao/AutoMapper.java
@@ -1,11 +1,15 @@
 package team.project.one.dao;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 
 public interface AutoMapper {
-
-
-	ArrayList<String> getlist(HashMap<String, String> templist);
-
+	ArrayList<String> getlistall(Map<String, String[]> templist);
+	ArrayList<String> getlistcpu(Map<String, String[]> templist);
+	ArrayList<String> getlistgpu(Map<String, String[]> templist);
+	ArrayList<String> getlistram(Map<String, String[]> templist);
+	ArrayList<String> getlistmainboard(Map<String, String[]> templist);
+	ArrayList<String> getlistcase(Map<String, String[]> templist);
+	ArrayList<String> getlistpower(Map<String, String[]> templist);
+	ArrayList<String> getliststeam(Map<String, String[]> templist);
 }
